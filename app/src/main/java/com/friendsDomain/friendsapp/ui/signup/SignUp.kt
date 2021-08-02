18e4ba42b7e0ba878b28d215971203f76dc183e7
ernest75 <ernestschneider@gmail.com>
2021-08-02 14:11:28 +0200

    SignUp screen email and password fields logic

--- app/src/main/java/com/friendsDomain/friendsapp/ui/signup/SignUp.kt
+++ app/src/main/java/com/friendsDomain/friendsapp/ui/signup/SignUp.kt
@@ -1,15 +1,20 @@
 package com.friendsDomain.friendsapp.ui.signup
 
-import android.widget.Space
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
+import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.typography
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,30 +28,20 @@ fun SignUp() {
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = stringResource(id = R.string.createAccount),
-                style = typography.h4
-            )
-        }
+        ScreenTitle(R.string.createAccount)
         Spacer(modifier= Modifier.height(16.dp))
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = "",
-            label = {
-                Text(text = stringResource(id = R.string.email))
-            },
-            onValueChange = {})
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = "",
-            label = {
-                Text(text = stringResource(id = R.string.password))
-            },
-            onValueChange = {})
+        var email by remember {
+            mutableStateOf("")
+        }
+        EmailField(
+            value = email,
+            onValueChange = {email = it})
+        var password by remember {
+            mutableStateOf("")
+        }
+        PasswordField(
+            value = password,
+            onValueChange = {password = it})
         Spacer(modifier= Modifier.height(8.dp))
         Button(
             modifier = Modifier.fillMaxWidth(),
@@ -54,5 +49,56 @@ fun SignUp() {
         ) {
             Text(text = stringResource(id = R.string.signUp))
         }
+    }
+}
+
+@Composable
+private fun EmailField(
+    value: String,
+    onValueChange:(String)-> Unit
+) {
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = value,
+        label = {
+            Text(text = stringResource(id = R.string.email))
+        },
+        onValueChange = onValueChange)
+}
+
+@Composable
+private fun PasswordField(
+    value: String,
+    onValueChange:(String)-> Unit
+) {
+    var isVisible by remember { mutableStateOf(false) }
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = value,
+        trailingIcon = {
+            IconButton(onClick = { isVisible = !isVisible }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_visibility),
+                    contentDescription = stringResource(id = R.string.toogleVisibility)
+                )
+            }
+        },
+        visualTransformation = if (isVisible) VisualTransformation.None else PasswordVisualTransformation(),
+        label = {
+            Text(text = stringResource(id = R.string.password))
+        },
+        onValueChange = onValueChange)
+}
+
+@Composable
+private fun ScreenTitle(@StringRes resource: Int) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(resource),
+            style = typography.h4
+        )
     }
 }
