b203a1163c29e1474a932b8d3bc2922f86aeaa5a
ernest75 <ernestschneider@gmail.com>
2021-08-09 16:34:54 +0200

    Add FriendsApplication.kt custom app class

---
+++ app/src/main/java/com/friendsDomain/friendsapp/app/FriendsApplication.kt
@@ -0,0 +1,10 @@
+package com.friendsDomain.friendsapp.app
+
+import android.app.Application
+
+class FriendsApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
