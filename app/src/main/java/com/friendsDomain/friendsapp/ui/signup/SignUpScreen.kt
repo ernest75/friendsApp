b6218b5b810cc055cfd0076ff49bcff819cc759c
ernest75 <ernestschneider@gmail.com>
2021-08-10 17:43:18 +0200

    Merged if conditions in SignUpScreen.kt

--- app/src/main/java/com/friendsDomain/friendsapp/ui/signup/SignUpScreen.kt
+++ app/src/main/java/com/friendsDomain/friendsapp/ui/signup/SignUpScreen.kt
@@ -35,6 +35,16 @@ fun SignUpScreen(
 
     if (signUpState is SignUpState.SignedUp) {
         onSignedUp()
+    } else    if (signUpState is SignUpState.BadEmail){
+        isBadEmail = true
+    } else if (signUpState is SignUpState.BadPassword){
+        isBadPassword = true
+    } else if (signUpState is SignUpState.DuplicateAccount) {
+        InfoMessage(R.string.duplicateAccountError)
+    } else if (signUpState is SignUpState.BackEndError){
+        InfoMessage(stringResource = R.string.createAccountError)
+    } else if (signUpState is SignUpState.Offline){
+        InfoMessage(stringResource = R.string.offLineError)
     }
     Box(modifier = Modifier.fillMaxSize()) {
         Column(
@@ -67,17 +77,6 @@ fun SignUpScreen(
             ) {
                 Text(text = stringResource(id = R.string.signUp))
             }
-        }
-        if (signUpState is SignUpState.BadEmail){
-            isBadEmail = true
-        } else if (signUpState is SignUpState.BadPassword){
-            isBadPassword = true
-        } else if (signUpState is SignUpState.DuplicateAccount) {
-            InfoMessage(R.string.duplicateAccountError)
-        } else if (signUpState is SignUpState.BackEndError){
-            InfoMessage(stringResource = R.string.createAccountError)
-        } else if (signUpState is SignUpState.Offline){
-            InfoMessage(stringResource = R.string.offLineError)
         }
     }
 }
