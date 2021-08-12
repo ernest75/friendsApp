108d754aed6b0d396785ba63f9cfa6ebb48e1f21
ernest75 <ernestschneider@gmail.com>
2021-08-12 17:10:22 +0200

    Pull up variables on RenderingSignupStatesTest.kt

--- app/src/test/java/com/friendsDomain/friendsapp/signup/RenderingSignupStatesTest.kt
+++ app/src/test/java/com/friendsDomain/friendsapp/signup/RenderingSignupStatesTest.kt
@@ -15,11 +15,12 @@ import org.junit.jupiter.api.extension.ExtendWith
 @ExtendWith(InstantTaskExecutorExtension::class)
 class RenderingSignupStatesTest {
 
+    private val userRepository = UserRepository(InMemoryUserCatalog())
+    private val viewModel = SignUpViewModel(RegexCredentialsValidator(), userRepository)
+    private val tom = User("tomId", "tom@friends.com", "about Tom")
+
     @Test
     fun uiStatesAreDeliveredInParticularOrder() {
-        val userRepository = UserRepository(InMemoryUserCatalog())
-        val viewModel = SignUpViewModel(RegexCredentialsValidator(), userRepository)
-        val tom = User("tomId", "tom@friends.com", "about Tom")
         val deliveredStates = mutableListOf<SignUpState>()
         viewModel.signUpState.observeForever { deliveredStates.add(it) }
 
