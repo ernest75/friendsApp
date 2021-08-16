05acffdf1e52379771f3cf99b61efa7890a17a7c
ernest75 <ernestschneider@gmail.com>
2021-08-16 19:39:30 +0200

    Moved InMemoryPostCatalog.kt to stand alone class

--- app/src/main/java/com/friendsDomain/friendsapp/timeline/TimeLineViewModel.kt
+++ app/src/main/java/com/friendsDomain/friendsapp/timeline/TimeLineViewModel.kt
@@ -2,7 +2,7 @@ package com.friendsDomain.friendsapp.timeline
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.friendsDomain.friendsapp.domain.post.Post
+import com.friendsDomain.friendsapp.domain.post.InMemoryPostCatalog
 import com.friendsDomain.friendsapp.domain.user.Following
 import com.friendsDomain.friendsapp.timeline.state.TimelineState
 
@@ -26,20 +26,6 @@ class TimeLineViewModel {
         return followings
             .filter { it.userId == userId }
             .map { it.followedId }
-    }
-
-    class InMemoryPostCatalog{
-
-        fun postsFor(userIds: List<String>): List<Post> {
-            val availablePosts = listOf(
-                Post("postId", "timId", "post text", 1L),
-                Post("post2", "lucyId", "post 2", 2L),
-                Post("post1", "lucyId", "post 1", 1L),
-                Post("post4", "saraId", "post 4", 4L),
-                Post("post3", "saraId", "post 3", 3L)
-            )
-            return availablePosts.filter { userIds.contains(it.userId) }
-        }
     }
 
 }
