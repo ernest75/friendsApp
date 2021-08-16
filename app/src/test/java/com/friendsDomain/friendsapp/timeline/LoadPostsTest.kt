e42e1f9221ba7e5fc77734592cd7d38b04c063df
ernest75 <ernestschneider@gmail.com>
2021-08-16 18:20:30 +0200

    Can load posts from friends and own

--- app/src/test/java/com/friendsDomain/friendsapp/timeline/LoadPostsTest.kt
+++ app/src/test/java/com/friendsDomain/friendsapp/timeline/LoadPostsTest.kt
@@ -48,5 +48,28 @@ class LoadPostsTest {
             Post("post1", lucy.id, "post 1",1L)
         )
 
+        assertEquals(TimelineState.Posts(lucyPosts),
+            viewModel.timelineState.value)
+    }
+    
+    @Test
+    fun postFromFriendsAlongOwn() {
+        val lucy = aUser().withId("lucyId").build()
+        val lucyPosts = listOf(
+            Post("post2", lucy.id, "post 2", 2L),
+            Post("post1", lucy.id, "post 1",1L)
+        )
+        val sara = aUser().withId("saraId").build()
+        val saraPosts = listOf(
+            Post("post4",sara.id,"post 4",4L),
+            Post("post3",sara.id,"post 3",3L)
+        )
+
+        val viewModel = TimeLineViewModel()
+
+        viewModel.timelineFor(sara.id)
+
+        assertEquals(TimelineState.Posts(lucyPosts + saraPosts),
+            viewModel.timelineState.value)
     }
 }
