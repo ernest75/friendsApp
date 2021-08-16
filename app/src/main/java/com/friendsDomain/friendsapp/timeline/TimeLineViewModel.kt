747579fa6bacaab8ec457e9a2c7683cee6d1b769
ernest75 <ernestschneider@gmail.com>
2021-08-16 18:54:30 +0200

    Removed duplication

--- app/src/main/java/com/friendsDomain/friendsapp/timeline/TimeLineViewModel.kt
+++ app/src/main/java/com/friendsDomain/friendsapp/timeline/TimeLineViewModel.kt
@@ -16,25 +16,22 @@ class TimeLineViewModel {
             Post("postId", "timId", "post text", 1L),
             Post("post2", "lucyId", "post 2", 2L),
             Post("post1", "lucyId", "post 1", 1L),
-            Post("post4","saraId","post 4",4L),
-            Post("post3","saraId","post 3",3L)
+            Post("post4", "saraId", "post 4", 4L),
+            Post("post3", "saraId", "post 3", 3L)
         )
-        if (userId == "saraId"){
-            val lucyPosts = availablePosts
-                .filter { it.userId == "lucyId" }
-            val saraPosts = availablePosts
-                .filter { it.userId == "saraId"  }
-            mutableTimelineState.value = TimelineState.Posts(lucyPosts + saraPosts)
-        }
-        else if (userId=="annaId"){
-            val annaPosts = availablePosts.filter { it.userId=="lucyId" }
-            mutableTimelineState.value = TimelineState.Posts(annaPosts)
-        }
-        else if(userId=="timId"){
-            val timPosts = availablePosts.filter { it.userId=="timId" }
-            mutableTimelineState.value = TimelineState.Posts(timPosts)
-        }else{
-            mutableTimelineState.value = TimelineState.Posts(emptyList())
-        }
+        val followings = listOf(
+            Following("saraId", "lucyId"),
+            Following("annaId", "lucyId")
+        )
+        val userIds = listOf(userId) + followings
+            .filter { it.userId == userId }
+            .map { it.followedId }
+        val postsForSara = availablePosts.filter { userIds.contains(it.userId) }
+        mutableTimelineState.value = TimelineState.Posts(postsForSara)
     }
+
+    data class Following(
+        val userId: String,
+        val followedId:String
+    )
 }
