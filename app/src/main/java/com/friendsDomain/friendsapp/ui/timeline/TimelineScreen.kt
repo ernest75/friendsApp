69bedd69a01d4fe458db330a8c88eb512cc33558
ernest75 <ernestschneider@gmail.com>
2021-08-17 20:06:41 +0200

    Improved post UI list item

--- app/src/main/java/com/friendsDomain/friendsapp/ui/timeline/TimelineScreen.kt
+++ app/src/main/java/com/friendsDomain/friendsapp/ui/timeline/TimelineScreen.kt
@@ -1,20 +1,22 @@
 package com.friendsDomain.friendsapp.ui.timeline
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.friendsDomain.friendsapp.R
 import com.friendsDomain.friendsapp.domain.post.Post
 import com.friendsDomain.friendsapp.timeline.TimelineViewModel
-import com.friendsDomain.friendsapp.timeline.state.TimelineState
 import com.friendsDomain.friendsapp.timeline.state.TimelineState.*
 import com.friendsDomain.friendsapp.ui.composables.ScreenTitle
 
@@ -58,12 +60,38 @@ private fun PostsList(posts: List<Post>) {
         LazyColumn {
             items(posts){ post ->
                 PostItem(post)
+                Spacer(modifier = Modifier.height(16.dp))
             }
         }
     }
 }
 
 @Composable
-fun PostItem(post: Post) {
-    Text(text = post.postText)
+fun PostItem(
+    post: Post,
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier
+        .clip(shape = RoundedCornerShape(16.dp))
+        .fillMaxWidth()
+        .border(
+            width =  1.dp,
+            color = MaterialTheme.colors.onSurface,
+            shape = RoundedCornerShape(16.dp)
+        )
+    ){
+        Text(
+            text = post.postText,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PostsListPreview(){
+    val posts = (0..100).map { index ->
+        Post("$index", "user$index", "This is a post number $index", index.toLong())
+    }
+    PostsList(posts = posts)
 }
