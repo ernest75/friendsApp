d40e5a1ee741ab638f33f0095af4026af91e2842
ernest75 <ernestschneider@gmail.com>
2021-11-06 18:20:57 +0100

    Use exceptions to raise errors when creatingpost goes wrong

--- app/src/main/java/com/friendsDomain/friendsapp/postcomposer/CreatePostViewModel.kt
+++ app/src/main/java/com/friendsDomain/friendsapp/postcomposer/CreatePostViewModel.kt
@@ -2,11 +2,14 @@ package com.friendsDomain.friendsapp.postcomposer
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.friendsDomain.friendsapp.domain.exceptions.BackendException
+import com.friendsDomain.friendsapp.domain.exceptions.ConnectionUnavailableException
 import com.friendsDomain.friendsapp.domain.post.Post
 import com.friendsDomain.friendsapp.domain.user.InMemoryUserData
 import com.friendsDomain.friendsapp.infrastructure.Clock
 import com.friendsDomain.friendsapp.infrastructure.IdGenerator
 import com.friendsDomain.friendsapp.postcomposer.state.CreatePostState
+import com.friendsDomain.friendsapp.timeline.state.TimelineState
 
 class CreatePostViewModel(
     private val userData: InMemoryUserData,
@@ -18,17 +21,22 @@ class CreatePostViewModel(
     val postState: LiveData<CreatePostState> = mutablePostState
 
     fun createPost(postText: String) {
-        if (postText == ":backEnd:"){
-            mutablePostState.value = CreatePostState.BackEndError
-        } else if(postText == ":offline:"){
-            mutablePostState.value = CreatePostState.Offline
-        }else {
-            val post = addPost(userData.loggedInUserId(), postText)
+        try {
+            val post = addPost(userData.loggedInUserId(),postText)
             mutablePostState.value = CreatePostState.Created(post)
+        } catch (backendException: BackendException ){
+            mutablePostState.value = CreatePostState.BackEndError
+        } catch (connectionUnavailableException: ConnectionUnavailableException){
+            mutablePostState.value = CreatePostState.Offline
         }
     }
 
     private fun addPost(userId: String, postText: String): Post {
+        if (postText == ":backEnd:"){
+            throw BackendException()
+        } else if (postText == ":offline:"){
+            throw ConnectionUnavailableException()
+        }
         val timeStamp = clock.now()
         val postId = idGenerator.next()
         return Post(postId, userId, postText, timeStamp)
